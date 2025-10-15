58536f02a68796eca0be69b3e7b3fc9afae74bb8
Bhavya-PR <bhavyapandurangan494@gmail.com>
2025-10-15 10:40:56 +0530

    Completed Day 24.

---
+++ Day-24 Updated Snake game/scoreboard.py
@@ -0,0 +1,33 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT_TYPE_SCORE = ("Arial", 14, "normal")
+FONT_TYPE_GAME = ("Arial", 30, "bold")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.goto(-2,280)
+        self.color("white")
+        with open("data.txt", mode="r") as file:
+            self.high_score = int(file.read())
+        self.score = 0
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.write(arg=f"Score : {self.score} High Score : {self.high_score}", align=ALIGNMENT, font=FONT_TYPE_SCORE)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt","w") as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.update_score()
+
+    def increase_score(self):
+        self.score += 1
+        self.update_score()
---
+++ Day-24 Updated Snake game/snake.py
@@ -0,0 +1,56 @@
+from turtle import Turtle
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+POSITION = [(0,0),(-20,0),(-40,0)]
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for i in range(3):
+            self.add_segment(POSITION[i])
+
+    def add_segment(self,position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000 , 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        for seg_num in range(len(self.segments)-1,0,-1):
+            x_cor = self.segments[seg_num - 1].xcor()
+            y_cor = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(x_cor,y_cor)
+        self.head.forward(20)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def turn_left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def turn_right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
