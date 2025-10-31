7edc61c16428136138a1c15dcd8239dd618bb057
Bhavya-PR <bhavyapandurangan494@gmail.com>
2025-10-31 12:20:04 +0530

    Completed Day-31.

---
+++ Day-31 Flash Card Project/main.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from tkinter import *
+import random
+
+
+BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+data_dict = {}
+
+try:
+    data = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    og_data = pd.read_csv("data/french_words.csv")
+    data_dict = og_data.to_dict(orient="records")
+else:
+    data_dict = data.to_dict(orient="records")
+
+def change_word():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(data_dict)
+    canvas.itemconfig(card_image,image=front_img)
+    canvas.itemconfig(card_title,text = "French",fill="black")
+    canvas.itemconfig(card_word,text=current_card['French'],fill="black")
+    flip_timer = window.after(3000,func=flip_card)
+
+def is_known_card():
+    data_dict.remove(current_card)
+    print(len(data_dict))
+    df = pd.DataFrame(data_dict)
+    df.to_csv("data/words_to_learn.csv",index=False)
+    change_word()
+
+def flip_card():
+    canvas.itemconfig(card_image,image=back_img)
+    canvas.itemconfig(card_title,text="English",fill="white")
+    canvas.itemconfig(card_word,text=current_card['English'],fill="white")
+
+window = Tk()
+window.title("Flashy")
+window.config(background=BACKGROUND_COLOR,padx=50,pady=50)
+
+flip_timer = window.after(3000,func=flip_card)
+
+canvas = Canvas(width=800,height=526,bg=BACKGROUND_COLOR,highlightthickness=0)
+front_img = PhotoImage(file="images/card_front.png")
+back_img = PhotoImage(file="images/card_back.png")
+card_image = canvas.create_image(400, 263,image=front_img)
+card_title = canvas.create_text(400,150,text="",font=("Arial",40,"italic"))
+card_word = canvas.create_text(400,263,text="",font=("Arial",60,"italic bold"))
+canvas.grid(row=0,column=0,columnspan=2)
+
+# Buttons
+right_img = PhotoImage(file="images/right.png")
+right_button = Button(image=right_img,highlightthickness=0,command=is_known_card)
+right_button.grid(row=1,column=1)
+
+wrong_img = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong_img,highlightthickness=0,command=change_word)
+wrong_button.grid(row=1,column=0)
+
+change_word()
+
+window.mainloop()
